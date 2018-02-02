b3f3f5fb3586433febfa1f41c21291b5b788dda8
Sergey Ilichev <ilichev@mbk-capital.ru>
2018-02-02 08:40:18 +0000

    Добавил расширение для добавления языка в url. Продолжаю настраивать роутинг.

--- common/config/main.php
+++ common/config/main.php
@@ -6,8 +6,22 @@ return [
     ],
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
+        'urlManager' => [
+            'class' => 'codemix\localeurls\UrlManager',
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+            'rules' => [
+                '<>'
+            ],
+            'languages' => ['en', 'ru'],
+            'keepUppercaseLanguageCode' => true,
+        ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
     ],
+    // set target language to be Russian
+    'language' => 'ru',
+    // set source language to be Russian
+    'sourceLanguage' => 'ru',
 ];
